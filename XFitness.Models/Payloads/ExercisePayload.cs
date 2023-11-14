132207baa2c900bde117edeab35ce944acca32ea
Jędrzej Makuch <jedrek9291@gmail.com>
2023-11-14 22:57:51 +0100

    #1 Update Exercise Controller, Created ExerciseDto and ExercisePayload

---
+++ XFitness.Models/Payloads/ExercisePayload.cs
@@ -0,0 +1,11 @@
+﻿namespace XFitness.Models.Payloads
+{
+    public class ExercisePayload
+    {
+        public string ExerciseName { get; set; }
+        public string ExerciseDescription { get; set; }
+        public string ImageUrl { get; set; }
+        public string Category { get; set; }
+        public DateTime ExerciseCreatedDate { get; set; }
+    }
+}
